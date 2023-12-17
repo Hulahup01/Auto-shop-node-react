3aa20b8af5e899f81b61ae9f8984a7d6dec9d035
hulahup01 <ilya.deepak@yandex.ru>
2023-12-17 05:04:11 +0300

    well, its a start

---
+++ server/controllers/brandController.js
@@ -0,0 +1,23 @@
+class BrandController {
+    async create(req, res) {
+        
+    }
+
+    async update(req, res) {
+
+    }
+
+    async delete(req, res) {
+
+    }
+
+    async getAll(req, res) {
+        
+    }
+
+    async getById(req, res) {
+        
+    }
+}
+
+module.exports = new BrandController()
